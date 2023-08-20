90f69ba7b1c31d50b82b776bedaa0942d98263f6
Fatitboo <21522448@gm.uit.edu.vn>
2023-08-20 09:45:33 +0700

    add app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
+const app = require('./app/app.js');
 require('dotenv').config();
 
-http.createServer().listen(process.env.port, ()=>{
+http.createServer(app).listen(process.env.port, ()=>{
     console.log('Server is running at port '+ process.env.port);
 })
